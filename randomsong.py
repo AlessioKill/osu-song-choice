63b71c5090503485eeeb103f4d9f82ce1b29e7bb
Alessio <167238330+AlessioKill@users.noreply.github.com>
2025-03-28 17:43:04 +0100

    Add files via upload

---
+++ randomsong.py
@@ -0,0 +1,10 @@
+import os
+import random
+import os
+beatmaps = input("Put the path of the osu songs directory: ")
+if os.path.isdir(beatmaps):  # Controlla se la cartella esiste
+    file_nomi = os.listdir(beatmaps)
+    song = random.choice(file_nomi)
+    print("The song you must play is" + song)
+else:
+    print("The directory does not exist!")
