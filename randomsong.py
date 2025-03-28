55431d671a11f8b2c2e81ca68884c667f3f56192
Alessio <167238330+AlessioKill@users.noreply.github.com>
2025-03-28 17:43:31 +0100

    Update randomsong.py

--- randomsong.py
+++ randomsong.py
@@ -2,7 +2,7 @@ import os
 import random
 import os
 beatmaps = input("Put the path of the osu songs directory: ")
-if os.path.isdir(beatmaps):  # Controlla se la cartella esiste
+if os.path.isdir(beatmaps): 
     file_nomi = os.listdir(beatmaps)
     song = random.choice(file_nomi)
     print("The song you must play is" + song)
