792b25b90bd14a418549afea945bbdc1a7f66946
Alessio <167238330+AlessioKill@users.noreply.github.com>
2025-03-28 17:48:58 +0100

    Update randomsong.py

--- randomsong.py
+++ randomsong.py
@@ -1,10 +1,10 @@
 import os
 import random
 import os
-beatmaps = input("Put the path of the osu songs directory: ")
+beatmaps = input("Put the path of the osu songs directory (usually "C:\Games\osu!\Songs"): ")
 if os.path.isdir(beatmaps): 
     file_nomi = os.listdir(beatmaps)
     song = random.choice(file_nomi)
-    print("The song you must play is" + song)
+    print("The song you must play is " + song)
 else:
     print("The directory does not exist!")
